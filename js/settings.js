0aa509cc08e73da49e38c8c2f092ae82f164f707
Chris Kruining <chris@kruining.eu>
2019-01-27 22:48:19 +0100

    initial commit

---
+++ js/settings.js
@@ -0,0 +1,14 @@
+import * as Fyn from '/node_modules/@fyn-software/component/fyn.js';
+
+export default class Settings extends Fyn.Component
+{
+    static get properties()
+    {
+        return {
+        };
+    }
+
+    ready()
+    {
+    }
+}
